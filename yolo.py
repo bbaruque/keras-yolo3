acc609e8a67c92a8ee809571c9e88fb57f72613f
Bruno Baruque <bbaruque@ubu.es>
2019-11-07 17:41:18 +0100

    Comentado código que dibuja sobre la imágen

--- yolo.py
+++ yolo.py
@@ -20,9 +20,9 @@ from keras.utils import multi_gpu_model
 
 class YOLO(object):
     _defaults = {
-        "model_path": 'model_data/yolo.h5',
-        "anchors_path": 'model_data/yolo_anchors.txt',
-        "classes_path": 'model_data/coco_classes.txt',
+        "model_path": 'model_transfered/tiny_yolo.h5',
+        "anchors_path": 'model_transfered/tiny_yolo_anchors_heads.txt',
+        "classes_path": 'heads_dataset/labels/classes.txt',
         "score" : 0.3,
         "iou" : 0.45,
         "model_image_size" : (416, 416),
@@ -126,41 +126,41 @@ class YOLO(object):
 
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
 
-        font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
-                    size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
-        thickness = (image.size[0] + image.size[1]) // 300
+        # font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
+        #             size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+        # thickness = (image.size[0] + image.size[1]) // 300
 
-        for i, c in reversed(list(enumerate(out_classes))):
-            predicted_class = self.class_names[c]
-            box = out_boxes[i]
-            score = out_scores[i]
+        # for i, c in reversed(list(enumerate(out_classes))):
+        #     predicted_class = self.class_names[c]
+        #     box = out_boxes[i]
+        #     score = out_scores[i]
 
-            label = '{} {:.2f}'.format(predicted_class, score)
-            draw = ImageDraw.Draw(image)
-            label_size = draw.textsize(label, font)
+        #     label = '{} {:.2f}'.format(predicted_class, score)
+        #     draw = ImageDraw.Draw(image)
+        #     label_size = draw.textsize(label, font)
 
-            top, left, bottom, right = box
-            top = max(0, np.floor(top + 0.5).astype('int32'))
-            left = max(0, np.floor(left + 0.5).astype('int32'))
-            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
-            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
-            print(label, (left, top), (right, bottom))
+        #     top, left, bottom, right = box
+        #     top = max(0, np.floor(top + 0.5).astype('int32'))
+        #     left = max(0, np.floor(left + 0.5).astype('int32'))
+        #     bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+        #     right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+        #     print(label, (left, top), (right, bottom))
 
-            if top - label_size[1] >= 0:
-                text_origin = np.array([left, top - label_size[1]])
-            else:
-                text_origin = np.array([left, top + 1])
+        #     if top - label_size[1] >= 0:
+        #         text_origin = np.array([left, top - label_size[1]])
+        #     else:
+        #         text_origin = np.array([left, top + 1])
 
-            # My kingdom for a good redistributable image drawing library.
-            for i in range(thickness):
-                draw.rectangle(
-                    [left + i, top + i, right - i, bottom - i],
-                    outline=self.colors[c])
-            draw.rectangle(
-                [tuple(text_origin), tuple(text_origin + label_size)],
-                fill=self.colors[c])
-            draw.text(text_origin, label, fill=(0, 0, 0), font=font)
-            del draw
+        #     # My kingdom for a good redistributable image drawing library.
+        #     for i in range(thickness):
+        #         draw.rectangle(
+        #             [left + i, top + i, right - i, bottom - i],
+        #             outline=self.colors[c])
+        #     draw.rectangle(
+        #         [tuple(text_origin), tuple(text_origin + label_size)],
+        #         fill=self.colors[c])
+        #     draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+        #     del draw
 
         end = timer()
         print(end - start)
