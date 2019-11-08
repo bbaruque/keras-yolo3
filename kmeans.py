d67490fbc1647ff53a258f89d26d824ea0463fe3
Bruno Baruque <bbaruque@ubu.es>
2019-11-08 14:09:19 +0100

    Se añade otro conjunto de anchors para el modelo completo

--- kmeans.py
+++ kmeans.py
@@ -5,7 +5,7 @@ class YOLO_Kmeans:
 
     def __init__(self, cluster_number, filename):
         self.cluster_number = cluster_number
-        self.filename = "2012_train.txt"
+        self.filename = "../../datasets/labels/HeadsTraining_modif_onlyHeads.csv"
 
     def iou(self, boxes, clusters):  # 1 box -> k clusters
         n = boxes.shape[0]
@@ -58,7 +58,7 @@ class YOLO_Kmeans:
         return clusters
 
     def result2txt(self, data):
-        f = open("yolo_anchors.txt", 'w')
+        f = open("./model_transfered/yolo_anchors_heads.txt", 'w')
         row = np.shape(data)[0]
         for i in range(row):
             if i == 0:
@@ -96,6 +96,6 @@ class YOLO_Kmeans:
 
 if __name__ == "__main__":
     cluster_number = 9
-    filename = "2012_train.txt"
+    filename = "../../dataSets/labels/HeadsTraining_modif_onlyHeads.csv"
     kmeans = YOLO_Kmeans(cluster_number, filename)
     kmeans.txt2clusters()
